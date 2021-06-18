73fc271a14cc330a65b946026adaa3b14d1e5aee
lbadocha87 <63849341+lbadocha87@users.noreply.github.com>
2021-06-18 16:59:57 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Heading from './Heading';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  constructor() {
+    super();
+
+    this.state = {
+      classValue: 'red'
+    }
+  }
+
+
+  changeColor = (color) => {
+    this.setState({classValue: color})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+        <Heading textColor={this.state.classValue} />
+        
+        <button onClick={()=>this.changeColor('red')}>red</button>
+        <button onClick={()=>this.changeColor('green')}>green</button>
+        <button onClick={()=>this.changeColor('blue')}>blue</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Heading.js
@@ -0,0 +1,6 @@
+const Heading = props => {
+
+    return <h1 className={props.textColor}>Nagłówek komponent</h1>;
+}
+
+export default Heading;
